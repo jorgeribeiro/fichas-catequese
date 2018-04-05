64d2bee2e49e603a083071e66a310b0911652b09
Jorge Ribeiro <joorgemelo@gmail.com>
2018-04-05 20:42:00 -0300

    Mudanças no database

--- config/catequese_fichas.sql
+++ config/catequese_fichas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 31, 2018 at 04:14 PM
+-- Generation Time: Apr 06, 2018 at 01:39 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -29,14 +29,14 @@ USE `catequese_fichas`;
 --
 -- Table structure for table `ficha`
 --
--- Creation: Mar 31, 2018 at 12:04 AM
--- Last update: Mar 31, 2018 at 01:00 AM
--- Last check: Mar 31, 2018 at 01:07 AM
+-- Creation: Apr 04, 2018 at 01:14 AM
+-- Last update: Apr 04, 2018 at 01:43 AM
+-- Last check: Apr 05, 2018 at 11:38 PM
 --
 
 DROP TABLE IF EXISTS `ficha`;
-CREATE TABLE IF NOT EXISTS `ficha` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `ficha` (
+  `id` int(11) NOT NULL,
   `nome` varchar(128) NOT NULL,
   `data_nascimento` date NOT NULL,
   `naturalidade` varchar(32) NOT NULL,
@@ -69,9 +69,8 @@ CREATE TABLE IF NOT EXISTS `ficha` (
   `catequista_2` varchar(128) NOT NULL,
   `catequista_3` varchar(128) NOT NULL,
   `catequizando_frequente` tinyint(1) NOT NULL,
-  `preenchimento_ficha` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+  `preenchimento_ficha` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `ficha`
@@ -82,11 +81,76 @@ INSERT INTO `ficha` (`id`, `nome`, `data_nascimento`, `naturalidade`, `endereco`
 (4, 'Marco', '0000-00-00', '', '', '', 0, 0, 0, '', 0, 0, '0000-00-00', '', '', '', 0, 0, 0, 0, '', 0, 0, 0, '', '', '', '', 0, '', '', '', 0, 0),
 (5, 'Lucas', '0000-00-00', '', '', '', 0, 0, 0, '', 0, 0, '0000-00-00', '', '', '', 0, 0, 0, 0, '', 0, 0, 0, '', '', '', '', 0, '', '', '', 0, 0),
 (6, 'Nathalia', '0000-00-00', '', '', '', 0, 0, 0, '', 0, 0, '0000-00-00', '', '', '', 0, 0, 0, 0, '', 0, 0, 0, '', '', '', '', 0, '', '', '', 0, 0),
-(7, 'NatÃ¡lia', '0000-00-00', '', '', '', 0, 0, 0, '', 0, 0, '0000-00-00', '', '', '', 0, 0, 0, 0, '', 0, 0, 0, '', '', '', '', 0, '', '', '', 0, 0),
+(7, 'Natália', '0000-00-00', '', '', '', 0, 0, 0, '', 0, 0, '0000-00-00', '', '', '', 0, 0, 0, 0, '', 0, 0, 0, '', '', '', '', 0, '', '', '', 0, 0),
 (8, 'Sammyra', '0000-00-00', '', '', '', 0, 0, 0, '', 0, 0, '0000-00-00', '', '', '', 0, 0, 0, 0, '', 0, 0, 0, '', '', '', '', 0, '', '', '', 0, 0),
 (9, 'Matheus', '0000-00-00', '', '', '', 0, 0, 0, '', 0, 0, '0000-00-00', '', '', '', 0, 0, 0, 0, '', 0, 0, 0, '', '', '', '', 0, '', '', '', 0, 0),
-(12, 'AntÃ´nio', '0000-00-00', '', '', '', 0, 0, 0, '', 0, 0, '0000-00-00', '', '', '', 0, 0, 0, 0, '', 0, 0, 0, '', '', '', '', 0, '', '', '', 0, 0),
+(12, 'Antônio', '0000-00-00', '', '', '', 0, 0, 0, '', 0, 0, '0000-00-00', '', '', '', 0, 0, 0, 0, '', 0, 0, 0, '', '', '', '', 0, '', '', '', 0, 0),
 (11, 'Dilene', '0000-00-00', '', '', '', 0, 0, 0, '', 0, 0, '0000-00-00', '', '', '', 0, 0, 0, 0, '', 0, 0, 0, '', '', '', '', 0, '', '', '', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario`
+--
+-- Creation: Apr 04, 2018 at 01:14 AM
+-- Last update: Apr 04, 2018 at 01:43 AM
+-- Last check: Apr 05, 2018 at 11:38 PM
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(32) NOT NULL,
+  `sobrenome` varchar(32) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `telefone` varchar(64) NOT NULL,
+  `endereco` text NOT NULL,
+  `senha` varchar(512) NOT NULL,
+  `access_level` varchar(16) NOT NULL,
+  `access_code` text NOT NULL,
+  `status` int(11) NOT NULL COMMENT '0=pending,1=confirmed',
+  `criado` datetime NOT NULL,
+  `modificado` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='admin and customer users';
+
+--
+-- Dumping data for table `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nome`, `sobrenome`, `email`, `telefone`, `endereco`, `senha`, `access_level`, `access_code`, `status`, `criado`, `modificado`) VALUES
+(6, 'Jorge', 'Ribeiro', 'joorgemelo@gmail.com', '98 9 82341517', 'Endereço', '$2y$10$5WygMaSJyACKABPZ0BJHmeXoRdz3a7guLak1v7mIDwOXrIWkthJNu', 'Usuario', '', 1, '2018-04-03 22:29:52', '2018-04-04 01:30:06');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ficha`
+--
+ALTER TABLE `ficha`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ficha`
+--
+ALTER TABLE `ficha`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
